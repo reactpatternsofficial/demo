d683a52ca36d708e0eeeecb5730dfc6c24bef809
Chabli Mohammed Yassine <mychabli@octo.com>
2022-04-09 06:24:34 +0000

    redux fc connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch} from "./state/store";
 import {CHANGE_APP_THEME} from "./state/actions";
-import {preferencesSelector} from "./state/reducer";
+import {AppState} from "./state/reducer";
+import {connect} from "react-redux"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps extends AppState{
+    dispatch: AppDispatch
+}
+function App(props: AppProps) {
 
-    const dispatch = useDispatch<AppDispatch>();
-    const {theme} = useSelector(preferencesSelector);
+    const {preferences:{theme}, dispatch} =props;
 
     const switchTheme = ()=> {
         dispatch({type: CHANGE_APP_THEME, payload: theme === "LIGHT" ? "DARK": "LIGHT" });
     }
-
-  return (
-    <div className={theme} >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React patterns official
-        </p>
-        <input type="button" value="switch theme" onClick={switchTheme}/>
-      </header>
-    </div>
-  );
+    return (
+        <div className={theme} >
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    React patterns official
+                </p>
+                <input type="button" value="switch theme" onClick={switchTheme}/>
+            </header>
+        </div>
+    );
 }
-
-export default App;
+const mapStateToProps = (state: AppState) => ({  preferences: state.preferences});
+export default connect(mapStateToProps)(App);
