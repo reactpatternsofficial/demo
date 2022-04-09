624bbe59ec23441578a333f538176e404d3caa4e
Chabli Mohammed Yassine <mychabli@octo.com>
2022-04-09 05:13:32 +0000

    redux with class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch} from "./state/store";
 import {CHANGE_APP_THEME} from "./state/actions";
-import {preferencesSelector} from "./state/reducer";
+import {AppState, PreferencesState} from "./state/reducer";
+import {connect} from "react-redux"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-
-    const dispatch = useDispatch<AppDispatch>();
-    const {theme} = useSelector(preferencesSelector);
-
-    const switchTheme = ()=> {
-        dispatch({type: CHANGE_APP_THEME, payload: theme === "LIGHT" ? "DARK": "LIGHT" });
-    }
-
-  return (
-    <div className={theme} >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React patterns official
-        </p>
-        <input type="button" value="switch theme" onClick={switchTheme}/>
-      </header>
-    </div>
-  );
+interface AppProps extends PreferencesState{
+    dispatch: AppDispatch;
 }
+class App extends React.Component<AppProps, PreferencesState>{
+    constructor(props: any) {
+        super(props);
+        this.state= {theme: props.theme};
+    }
+    switchTheme = () => {
+        this.props.dispatch({type: CHANGE_APP_THEME, payload: this.state.theme==="LIGHT"? "DARK": "LIGHT"});
+    }
+    componentDidUpdate(prevProps: Readonly<PreferencesState>, prevState: Readonly<PreferencesState>, snapshot?: any) {
+        if (this.props.theme !== prevProps.theme) {
+            this.setState({theme: this.props.theme});
+        }
+    }
+    render() {
+        return (
+            <div className={this.state.theme} >
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        React patterns official
+                    </p>
+                    <input
 
-export default App;
+                        type="button"
+                        value={`swith theme ${this.state.theme}`}
+                        onClick={()=> {this.switchTheme()}}/>
+                </header>
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state: AppState) => (state.preferences);
+export default connect(mapStateToProps)(App);
+
+
--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -9,23 +9,24 @@ interface Action {
 export interface AppState {
     preferences: PreferencesState
 }
-type Theme = "DARK" | "LIGHT"
+export type Theme = "DARK" | "LIGHT"
 
 export interface PreferencesState  {
     theme: Theme
 }
 const initialState: PreferencesState  = {
-    theme: "DARK"
+    theme: "LIGHT"
 }
 
 export const preferencesSelector = (state: AppState) => state.preferences;
 
 export default function Reducer(state: PreferencesState = initialState, {type, payload}: Action ): PreferencesState {
 
+
+    console.log("reducer calling", type, payload);
     switch (type) {
         case CHANGE_APP_THEME:
             return {...state , theme: payload as Theme}
-
         default:
             return  state;
     }
